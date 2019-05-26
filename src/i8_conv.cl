8ae8690640db0ab993a53ca083c79772a7689f39
Alex Ostrovski <ostrovski.alex@gmail.com>
2019-05-26 21:18:51 +0300

    Implement i8/32 convolution

---
+++ src/i8_conv.cl
@@ -0,0 +1,149 @@
+/// Filter size.
+// #define FILTER_SIZE 3
+
+/// Bit shift used in scaling operations.
+// #define BIT_SHIFT 8
+
+#define MAX_POINT ((uint2)(-1, -1))
+
+/// Maps the output coordinate to an input one.
+uint2 map_to_input(
+    uint2 output_pt,
+    uint2 offsets,
+    uint2 strides,
+    uint4 pads,
+    uint2 signal_dims
+) {
+    uint2 without_pads = output_pt * strides + offsets;
+
+    if ((without_pads.x < pads.x) || (without_pads.y < pads.y)) {
+        return MAX_POINT;
+    }
+
+    uint2 input_pt = without_pads - pads.xy;
+    if ((input_pt.x >= signal_dims.x) || (input_pt.y >= signal_dims.y)) {
+        return MAX_POINT;
+    }
+
+    return input_pt;
+}
+
+struct __attribute__((packed)) Params {
+    uint2 strides;
+    uint4 pads;
+    int scale;
+    int output_bias;
+    int signal_bias;
+    int filter_bias;
+};
+
+// Round to nearest, ties to even integer bit shift to the right.
+int rounded_rshift(int number) {
+    const int MASK = (1 << BIT_SHIFT) - 1;
+    const int THRESHOLD = 1 << (BIT_SHIFT - 1);
+
+    int lower_bits = number & MASK;
+    int result = number >> BIT_SHIFT;
+    // We assume `lower_bits >= 0`.
+
+    if ((lower_bits > THRESHOLD) || ((lower_bits == THRESHOLD) && ((result & 1) == 1))) {
+        result += 1;
+    }
+    return result;
+}
+
+/// Computes convolution of two `i8` (or `char`, in OpenCL C terms) tensors.
+///
+/// - `signal` should have `HxWxC` layout (i.e., the channel dimension is the inner-most one).
+/// - `filters` should have `MxK_HxK_WxC` layout, where `M` is the number of filters,
+///   `K_H` and `K_W` are spatial dimensions of a filter, `C` is the number of input channels.
+///
+/// The output will have form `MxH'xW'`.
+///
+/// The kernel should be launched with `[K_H * H', K_W * W', C]` workgroups,
+/// with `[K_H, K_W]` tasks per group. Each group will compute a single output point.
+__kernel void conv(
+    __global char *convolved,
+    __constant char *signal,
+    uint3 signal_dims,
+    __constant char *filters,
+    __constant int *filter_biases,
+    struct Params params
+) {
+    size_t x = get_group_id(0);
+    size_t y = get_group_id(1);
+    size_t filter = get_group_id(2);
+    size_t convolved_h = get_num_groups(0);
+    size_t convolved_w = get_num_groups(1);
+
+    uint2 offset = (uint2)(get_local_id(0), get_local_id(1));
+
+    int sum = 0;
+    uint2 input_pt = map_to_input(
+        (uint2)(x, y),
+        offset,
+        params.strides,
+        params.pads,
+        signal_dims.xy
+    );
+    if ((input_pt.x != -1) && (input_pt.y != -1)) {
+        size_t signal_offset =
+            input_pt.x * signal_dims.y * signal_dims.z +
+            input_pt.y * signal_dims.z;
+        size_t filter_offset =
+            filter * FILTER_SIZE * FILTER_SIZE * signal_dims.z +
+            offset.x * FILTER_SIZE * signal_dims.z +
+            offset.y * signal_dims.z;
+
+        int8 vec_sum = (int8)(0);
+        __constant char8 *signal_channels = (__constant char8*) &signal[signal_offset];
+        __constant char8 *filter_channels = (__constant char8*) &filters[filter_offset];
+        for (size_t i = 0; i < signal_dims.z / 8; i++) {
+            int8 signal_val = convert_int8(signal_channels[i]) + params.signal_bias;
+            int8 filter_val = convert_int8(filter_channels[i]) + params.filter_bias;
+            vec_sum = mad24(signal_val, filter_val, vec_sum);
+        }
+        sum = vec_sum.s0 + vec_sum.s1 + vec_sum.s2 + vec_sum.s3
+            + vec_sum.s4 + vec_sum.s5 + vec_sum.s6 + vec_sum.s7;
+
+        // Add remaining elements without vectorization.
+        __constant char *signal_channels_ = &signal[signal_offset];
+        __constant char *filter_channels_ = &filters[filter_offset];
+        for (size_t i = signal_dims.z & ~7; i < signal_dims.z; i++) {
+            sum = mad24(
+                params.signal_bias + signal_channels_[i],
+                params.filter_bias + filter_channels_[i],
+                sum
+            );
+        }
+    }
+
+    __local float results[FILTER_SIZE][FILTER_SIZE];
+    results[offset.x][offset.y] = sum;
+
+    // Wait for all workers in the group to submit their results.
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    if ((offset.x == 0) && (offset.y == 0)) {
+        int final_sum = 0;
+
+        for (size_t o_x = 0; o_x < FILTER_SIZE; o_x++) {
+            for (size_t o_y = 0; o_y < FILTER_SIZE; o_y++) {
+                final_sum += results[o_x][o_y];
+            }
+        }
+
+        // Perform a linear transform.
+        final_sum *= params.scale;
+        if (filter_biases != NULL) {
+            final_sum += filter_biases[filter];
+        }
+        final_sum = rounded_rshift(final_sum) + params.output_bias;
+
+        size_t output_offset =
+            filter * convolved_h * convolved_w +
+            x * convolved_w +
+            y;
+        convolved[output_offset] = convert_char_sat(final_sum);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ use ocl::{
 use std::{borrow::Cow, convert::TryFrom};
 
 const SOURCE: &str = include_str!("conv.cl");
+const I8_SOURCE: &str = include_str!("i8_conv.cl");
 
 /// Convolution of a specific filter size.
 #[derive(Debug)]
@@ -134,6 +135,317 @@ impl Convolution {
         }
 
         let mut output_data = vec![0.0_f32; output_buffer.len()];
+        output_buffer.read(&mut output_data).enq()?;
+        let output = Array3::from_shape_vec([filter_count, out_h, out_w], output_data).unwrap();
+        Ok(output)
+    }
+}
+
+/// Params for `I8Convolution`.
+///
+/// See [`I8Convolution`] docs for details how to set these parameters.
+///
+/// [`I8Convolution`]: struct.I8Convolution.html
+#[derive(Debug, Clone, Copy)]
+pub struct I8Params {
+    /// Strides (spacial distances between sequential application of filters).
+    pub strides: [usize; 2],
+    /// Pads for the signal.
+    pub pads: [usize; 4],
+    /// Fixed-point scale of the post-convolution transform.
+    pub scale: i32,
+    /// Bias for the post-convolution transform.
+    pub output_bias: i32,
+    /// Bias for the signal.
+    pub signal_bias: i32,
+    /// Bias for the filters.
+    pub filter_bias: i32,
+}
+
+impl I8Params {
+    /// Converts `scale` to fixed-point presentation. The resulting value can be used
+    /// as the `scale` field.
+    pub fn convert_scale(bit_shift: u8, scale: f32) -> i32 {
+        (((1 << i32::from(bit_shift)) as f32) * scale).round() as i32
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy, PartialEq)]
+#[repr(C, packed)]
+struct ClI8Params {
+    strides: Uint2,
+    pads: Uint4,
+    scale: i32,
+    output_bias: i32,
+    signal_bias: i32,
+    filter_bias: i32,
+}
+
+impl From<I8Params> for ClI8Params {
+    fn from(value: I8Params) -> Self {
+        ClI8Params {
+            strides: Uint2::new(value.strides[0] as u32, value.strides[1] as u32),
+            pads: Uint4::new(
+                value.pads[0] as u32,
+                value.pads[1] as u32,
+                value.pads[2] as u32,
+                value.pads[3] as u32,
+            ),
+            scale: value.scale,
+            output_bias: value.output_bias,
+            signal_bias: value.signal_bias,
+            filter_bias: value.filter_bias,
+        }
+    }
+}
+
+// Safety ensured by the same alignment here and in OCL code.
+unsafe impl ocl::OclPrm for ClI8Params {}
+
+/// Quantized convolution over signed 8-bit integers with the specified filter size.
+///
+/// ## Connection to real-value convolution
+///
+/// Quantized convolution mirrors real-valued convolution in which `i8` elements
+/// of the signal, filter and output tensors represent real-valued numbers with the
+/// following mapping:
+///
+/// ```
+/// let scale: f32 = // ...
+/// # 1.0;
+/// let bias: i32 = // ...
+/// # 0; drop(
+/// |x: i8| -> f32 { scale * (i32::from(x) - bias) as f32 }
+/// # )
+/// ```
+///
+/// `scale` and `bias` may differ for different tensors; these params are usually determined
+/// by *profiling* the corresponding convolutional neural network (see e.g. [this paper]).
+///
+/// Due to use of `i8` inputs, computations are performed much faster than on `f32` inputs
+/// (the difference manifests most on the specialized hardware, but it is seen in this
+/// OpenCL-powered implementation as well).
+///
+/// Denote these quantiation params for tensor `T` as `T.scale` and `T.bias`. Denote `S`
+/// the signal, `F` the filter, `O` the output. Convolution parameters should be set as follows:
+///
+/// | `I8Params` field | Value     |
+/// |------------------|-----------|
+/// | `signal_bias`    | `-S.bias` |
+/// | `filter_bias`    | `-F.bias` |
+/// | `output_bias`    | `O.bias`  |
+/// | `scale`          | `S.scale * F.scale / O.scale` |
+///
+/// `scale` is represented as a fixed-point number with [`bit_shift()`] binary digits after
+/// the point. Note that filter biases `B` are assumed to be unbiased and upscaled; they are
+/// not transformed during the computation.
+///
+/// # Computing convolution
+///
+/// Suppose `S` is the signal and `F` is the filter tensor; both contain `i8` values.
+/// The computation is performed as follows:
+///
+/// 1. Unbias the signal: `S := S + params.signal_bias`.
+/// 2. Unbias the filters: `F := F + params.filter_bias`.
+/// 3. Compute "standard" convolution output `O := S (*) F` using `i32` precision.
+/// 4. Upscale each number in the output: `O := O * params.scale`.
+/// 5. If there is filter bias `B` provided, apply bias to the output per each output channel:
+///    `O[f, ..] := O[f, ..] + B[f]`.
+/// 6. Downscale the output: `O := round(O / 2**self.bit_shift)`,
+///   where `round()` works as floating-point rounding with the default mode
+///   (round to nearest, ties to even).
+/// 7. Apply output bias: `O := O + params.output_bias`.
+/// 8. Saturate output to `i8` range.
+///
+/// [`bit_shift()`]: #method.bit_shift
+/// [this paper]: https://arxiv.org/abs/1805.00907
+///
+/// # Examples
+///
+/// ```
+/// use ndarray::{Array3, Array4};
+/// use rand::{Rng, thread_rng};
+/// use std::iter;
+/// # use ocl_convolution::{I8Convolution, I8Params};
+///
+/// # fn main() -> Result<(), ocl::Error> {
+/// const BIT_SHIFT: u8 = 16;
+/// let convolution = I8Convolution::new(3, BIT_SHIFT)?;
+/// // Generate random signal with 6x6 spacial dims and 3 channels.
+/// let mut rng = thread_rng();
+/// let mut signal = Array3::zeros([6, 6, 3]);
+/// signal.map_mut(|x| *x = rng.gen_range(-127, 127));
+/// // Construct two 3x3 spacial filters.
+/// let mut filters = Array4::zeros([2, 3, 3, 3]);
+/// filters.map_mut(|x| *x = rng.gen_range(-127, 127));
+/// // Perform the convolution. The output should have 4x4 spacial dims
+/// // and contain 2 channels (1 per each filter).
+/// let output = convolution.compute(
+///     signal.view(),
+///     filters.view(),
+///     None, // no filter biases
+///     I8Params {
+///         strides: [1, 1],
+///         pads: [0; 4],
+///         // These params are found by profiling; here, they are
+///         // chosen randomly.
+///         scale: I8Params::convert_scale(BIT_SHIFT, 0.1),
+///         output_bias: -10,
+///         signal_bias: 20,
+///         filter_bias: -5,
+///     },
+/// )?;
+/// assert_eq!(output.shape(), [2, 4, 4]);
+/// # Ok(())
+/// # }
+/// ```
+#[derive(Debug)]
+pub struct I8Convolution {
+    size: usize,
+    bit_shift: u8,
+    program: ProQue,
+}
+
+impl I8Convolution {
+    /// Creates a convolution with the specified size and bit shift.
+    pub fn new(size: usize, bit_shift: u8) -> ocl::Result<Self> {
+        assert_eq!(size % 2, 1, "Even convolution sizes are not supported");
+
+        let src = format!(
+            "#define FILTER_SIZE {}\n#define BIT_SHIFT {}\n{}",
+            size, bit_shift, I8_SOURCE
+        );
+        let program = ProQue::builder().src(src).build()?;
+        Ok(Self {
+            size,
+            bit_shift,
+            program,
+        })
+    }
+
+    /// Spatial size of the convolution.
+    pub fn size(&self) -> usize {
+        self.size
+    }
+
+    /// Bit shift used in fixed-point number operations.
+    pub fn bit_shift(&self) -> u8 {
+        self.bit_shift
+    }
+
+    /// Performs convolution on the provided `signal` and `filters`.
+    ///
+    /// # Parameters
+    ///
+    /// - `signal` should have `HxWxC` layout (i.e., the channel dimension is the inner-most one).
+    /// - `filters` should have `MxK_HxK_WxC` layout, where `M` is the number of filters,
+    ///   `K_H` and `K_W` are spatial dimensions of a filter, `C` is the number of input channel.
+    /// - `filter_biases`, if present, should have `M` elements.
+    ///
+    /// # Return value
+    ///
+    /// The output will have form `MxH'xW'`. An error means something wrong with OpenCL.
+    ///
+    /// # Panics
+    ///
+    /// - The method will panic if `filters` do not have expected spatial dimensions, i.e.,
+    ///   `self.size() x self.size()`.
+    /// - Likewise, the method will panic if the number of input channels differs from number of
+    ///   channels in `filters`, or if `filter_biases` length does not agree
+    ///   with the `filter` dimensions.
+    pub fn compute(
+        &self,
+        signal: ArrayView3<i8>,
+        filters: ArrayView4<i8>,
+        filter_biases: Option<&[i32]>,
+        params: I8Params,
+    ) -> ocl::Result<Array3<i8>> {
+        let in_h = signal.shape()[0];
+        let in_w = signal.shape()[1];
+        let in_channels = signal.shape()[2];
+        let filter_count = filters.shape()[0];
+
+        let pads = params.pads;
+        let strides = params.strides;
+        let out_h = (in_h - self.size + pads[0] + pads[2] + strides[0]) / strides[0];
+        let out_w = (in_w - self.size + pads[1] + pads[3] + strides[1]) / strides[1];
+
+        assert!(
+            filters.shape()[1] == self.size && filters.shape()[2] == self.size,
+            "Invalid filter shape"
+        );
+        assert_eq!(
+            in_channels,
+            filters.shape()[3],
+            "Channel dimensionality in signal and filters must agree"
+        );
+        if let Some(ref biases) = filter_biases {
+            assert_eq!(
+                biases.len(),
+                filter_count,
+                "Channel biases should have the same number of channels as the output"
+            );
+        }
+
+        let program = &self.program;
+
+        let signal_slice = signal
+            .as_slice()
+            .map(Cow::Borrowed)
+            .unwrap_or_else(|| Cow::Owned(signal.iter().cloned().collect()));
+        let signal_buffer = Buffer::builder()
+            .queue(program.queue().clone())
+            .len(<[usize; 3]>::try_from(signal.shape()).unwrap())
+            .flags(flags::MEM_READ_ONLY)
+            .copy_host_slice(signal_slice.as_ref())
+            .build()?;
+
+        let filters_slice = filters
+            .as_slice()
+            .map(Cow::Borrowed)
+            .unwrap_or_else(|| Cow::Owned(filters.iter().cloned().collect()));
+        let filters_buffer = Buffer::builder()
+            .queue(program.queue().clone())
+            .len(filters.shape().iter().product::<usize>())
+            .flags(flags::MEM_READ_ONLY)
+            .copy_host_slice(filters_slice.as_ref())
+            .build()?;
+
+        let output_buffer = Buffer::builder()
+            .queue(program.queue().clone())
+            .len([filter_count, out_h, out_w])
+            .flags(flags::MEM_HOST_READ_ONLY)
+            .build()?;
+
+        let kernel = program
+            .kernel_builder("conv")
+            .global_work_size([out_h * self.size, out_w * self.size, filter_count])
+            .local_work_size([self.size, self.size])
+            .arg_named("convolved", &output_buffer)
+            .arg_named("signal", &signal_buffer)
+            .arg_named(
+                "signal_dims",
+                Uint3::new(in_h as u32, in_w as u32, in_channels as u32),
+            )
+            .arg_named("filters", &filters_buffer)
+            .arg_named("filter_biases", None::<&Buffer<i32>>)
+            .arg_named("params", ClI8Params::from(params))
+            .build()?;
+        if let Some(biases) = filter_biases {
+            let biases = Buffer::builder()
+                .queue(program.queue().clone())
+                .len(biases.len())
+                .flags(flags::MEM_READ_ONLY)
+                .copy_host_slice(biases)
+                .build()?;
+            kernel.set_arg("filter_biases", &biases)?;
+        }
+
+        unsafe {
+            kernel.enq()?;
+        }
+
+        let mut output_data = vec![0; output_buffer.len()];
         output_buffer.read(&mut output_data).enq()?;
         let output = Array3::from_shape_vec([filter_count, out_h, out_w], output_data).unwrap();
         Ok(output)
@@ -278,5 +590,153 @@ mod tests {
 
         assert!((output[[0, 0, 0]] - 48.0).abs() < f32::EPSILON);
         // 48 = 4 * (0 + 1 + 5 + 6), numbers in the upper left corner of the image.
+    }
+
+    #[test]
+    fn rounding_in_i8_convolution() {
+        const BIT_SHIFT: u8 = 8;
+        let convolution = I8Convolution::new(1, BIT_SHIFT).unwrap();
+        let signal = Array3::from_shape_vec([2, 3, 1], vec![-7, -6, -5, 5, 6, 7]).unwrap();
+        let filter = Array4::from_shape_vec([1, 1, 1, 1], vec![1]).unwrap();
+        let params = I8Params {
+            strides: [1, 1],
+            pads: [0; 4],
+            scale: I8Params::convert_scale(BIT_SHIFT, 0.5),
+            output_bias: 0,
+            signal_bias: 0,
+            filter_bias: 0,
+        };
+
+        let output = convolution
+            .compute(signal.view(), filter.view(), None, params)
+            .unwrap();
+        let expected_output = Array3::from_shape_vec([1, 2, 3], vec![-4, -3, -2, 2, 3, 4]).unwrap();
+        assert_eq!(output, expected_output);
+    }
+
+    #[test]
+    fn i8_convolution() {
+        const BIT_SHIFT: u8 = 8;
+        let convolution = I8Convolution::new(3, BIT_SHIFT as u8).unwrap();
+
+        let signal = vec![
+            0, 1, 2, 3, 4, //
+            5, 6, 7, 8, 9, //
+            10, 11, 12, 13, 14, //
+            -5, -6, -7, -8, -9, //
+            0, -1, -2, -3, -4, //
+        ];
+        let signal = Array3::from_shape_vec([5, 5, 1], signal).unwrap();
+        let filter = Array4::from_shape_vec([1, 3, 3, 1], vec![1; 9]).unwrap();
+
+        let expected_output = vec![
+            54, 63, 72, //
+            33, 36, 39, //
+            12, 9, 6, //
+        ];
+        let expected_output = Array3::from_shape_vec([1, 3, 3], expected_output).unwrap();
+
+        let params = I8Params {
+            strides: [1, 1],
+            pads: [0; 4],
+            scale: I8Params::convert_scale(BIT_SHIFT, 1.0),
+            output_bias: 0,
+            signal_bias: 0,
+            filter_bias: 0,
+        };
+        let output = convolution
+            .compute(signal.view(), filter.view(), None, params)
+            .unwrap();
+        assert_eq!(output, expected_output);
+
+        // Check the same convolution with different scale / bias params.
+        // We use post-conv transform |x| { x / 3 - 12 }.
+        let expected_output = vec![
+            6, 9, 12, //
+            -1, 0, 1, //
+            -8, -9, -10, //
+        ];
+        let expected_output = Array3::from_shape_vec([1, 3, 3], expected_output).unwrap();
+
+        let params = I8Params {
+            strides: [1, 1],
+            pads: [0; 4],
+            scale: I8Params::convert_scale(BIT_SHIFT, 1.0 / 3.0),
+            output_bias: -12,
+            signal_bias: 0,
+            filter_bias: 0,
+        };;
+        let output = convolution
+            .compute(signal.view(), filter.view(), None, params)
+            .unwrap();
+        assert_eq!(output, expected_output);
+
+        // Check `filter_bias` / `signal_bias`.
+        let signal = vec![
+            0, 1, 2, 3, 4, //
+            5, 6, 7, 8, 9, //
+            10, 11, 12, 13, 14, //
+            -5, -6, -7, -8, -9, //
+            0, -1, -2, -3, -4, //
+        ];
+        let signal = Array3::from_shape_vec([5, 5, 1], signal).unwrap() - 7;
+        let filter = Array4::from_shape_vec([1, 3, 3, 1], vec![0; 9]).unwrap();
+
+        let params = I8Params {
+            strides: [1, 1],
+            pads: [0; 4],
+            output_bias: -12,
+            filter_bias: 1,
+            signal_bias: 7,
+            scale: I8Params::convert_scale(BIT_SHIFT, 1.0 / 3.0),
+        };
+        let output = convolution
+            .compute(signal.view(), filter.view(), None, params)
+            .unwrap();
+        assert_eq!(output, expected_output);
+    }
+
+    #[test]
+    fn i8_convolution_with_filter_bias() {
+        const BIT_SHIFT: u8 = 8;
+        const MULTIPLIER: i32 = 1 << (BIT_SHIFT as i32);
+
+        let convolution = I8Convolution::new(3, BIT_SHIFT).unwrap();
+
+        let signal = vec![
+            0, 1, 2, 3, 4, //
+            5, 6, 7, 8, 9, //
+            10, 11, 12, 13, 14, //
+            -5, -6, -7, -8, -9, //
+            0, -1, -2, -3, -4, //
+        ];
+        let signal = Array3::from_shape_vec([5, 5, 1], signal).unwrap();
+        let filter = Array4::from_shape_vec([2, 3, 3, 1], vec![1; 18]).unwrap();
+
+        let expected_output = vec![
+            // First filter output
+            6, 9, 12, //
+            -1, 0, 1, //
+            -8, -9, -10, //
+            // Second filter output
+            17, 20, 23, //
+            10, 11, 12, //
+            3, 2, 1, //
+        ];
+        let expected_output = Array3::from_shape_vec([2, 3, 3], expected_output).unwrap();
+
+        let params = I8Params {
+            strides: [1, 1],
+            pads: [0; 4],
+            scale: I8Params::convert_scale(BIT_SHIFT, 1.0 / 3.0),
+            output_bias: 0,
+            signal_bias: 0,
+            filter_bias: 0,
+        };
+        let biases = &[-12 * MULTIPLIER, -MULTIPLIER];
+        let output = convolution
+            .compute(signal.view(), filter.view(), Some(biases), params)
+            .unwrap();
+        assert_eq!(output, expected_output);
     }
 }
